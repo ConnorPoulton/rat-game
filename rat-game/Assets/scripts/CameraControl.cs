fdb50ed508621f39036c4f6bba7b0abf520b98f8
ConnorPoulton <poulton.cw@gmail.com>
2018-09-11 16:16:42 -0400

    Initial commit

---
+++ rat-game/Assets/scripts/CameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform anchor;
+	public float horizObl;
+	public float vertObl; 
+	public float zFromAnchor;
+	public float yFromAnchor;
+	private Vector3 updatedCameraPosition;
+
+	// Use this for initialization
+	void Start () {
+		SetObliqueness (horizObl, vertObl);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		updatedCameraPosition = anchor.position + new Vector3 (0, yFromAnchor, zFromAnchor);
+		Camera.main.transform.position = updatedCameraPosition;
+		Camera.main.transform.LookAt (anchor.position);
+	}
+
+	void SetObliqueness(float horizObl, float vertObl) {
+		Matrix4x4 mat  = Camera.main.projectionMatrix;
+		mat[0, 2] = horizObl;
+		mat[1, 2] = vertObl;
+		Camera.main.projectionMatrix = mat;
+	}
+}
---
+++ rat-game/Assets/scripts/RatMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatMovement : MonoBehaviour {
+	//public facing variables
+	public float maxVelocity;
+	public float P_xRotAccelDeg;
+	private float xRotAccelDelta;
+	public float P_xRotVelocDegMax;
+	private float xRotVeloc;
+	private Vector3 locationVector; 
+
+	// Use this for initialization
+	void Start () {
+		locationVector = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		applyVelocity ();
+	}
+
+
+	void applyVelocity(){
+		//Update rotation
+		float inputXAxis = Input.GetAxis("Horizontal");
+		Debug.Log(inputXAxis);
+		xRotAccelDelta = (P_xRotAccelDeg) * Time.deltaTime;
+		xRotVeloc = Mathf.Lerp (-inputXAxis, inputXAxis, xRotAccelDelta);
+		Quaternion quatRotation = Quaternion.Euler (xRotVeloc,0,0);
+		//update position
+		this.transform.position += ((quatRotation * Vector3.forward) * maxVelocity);//order matters here! 
+		//change rotation of model
+		float step = xRotVeloc * Time.deltaTime;
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, quatRotation, step);
+	}
+}
